5801631c5f78727454772701c19fa931f5cb87b5
ajay <ajay@gmail.com>
2025-05-20 17:13:52 +0530

    dashboard

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,29 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.custom-dashboard .tab');
+
     tabs.forEach(tab => {
-        tab.addEventListener('click', e => {
-            tabs.forEach(t => t.classList.remove('active', 'shadow-lg'));
-            tab.classList.add('active', 'shadow-lg');
+        tab.addEventListener('click', () => {
+            tabs.forEach(t => {
+                if (t !== tab) {
+                    t.classList.remove(
+                        'bg-white', 'shadow-xs', 'text-font-important-light', 'hover:bg-white',
+                        'dark:bg-base-900', 'dark:hover:bg-base-900', 'dark:text-font-important-dark'
+                    );
+                    t.classList.add(
+                        'text-font-subtle-light', 'dark:text-font-subtle-dark',
+                        'hover:bg-base-700/[.04]', 'dark:hover:bg-white/[.04]'
+                    );
+                }
+            });
+
+            tab.classList.remove(
+                'text-font-subtle-light', 'dark:text-font-subtle-dark',
+                'hover:bg-base-700/[.04]', 'dark:hover:bg-white/[.04]'
+            );
+            tab.classList.add(
+                'bg-white', 'shadow-xs', 'text-font-important-light', 'hover:bg-white',
+                'dark:bg-base-900', 'dark:hover:bg-base-900', 'dark:text-font-important-dark'
+            );
         });
     });
-})
+});
